c098a41cf2703f787b948b4f9597a613c95852b7
Oleh-VV <v.oleg.v@ukr.net>
2022-11-22 17:37:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
 import React from "react";
-import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("DIVRED");
-
   return (
     <div>
       <h1>QWERTY</h1>
